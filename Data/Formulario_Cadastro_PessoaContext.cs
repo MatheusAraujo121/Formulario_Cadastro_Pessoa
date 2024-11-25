487da58063c44fccb159b7e65c1016ee85342c8b
Matheus <mathujo453@gmail.com>
2024-11-25 14:44:29 -0300

    Adicionar arquivos de projeto.

---
+++ Data/Formulario_Cadastro_PessoaContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Formulario_Cadastro_Pessoa.Models;
+
+namespace Formulario_Cadastro_Pessoa.Data
+{
+    public class Formulario_Cadastro_PessoaContext : DbContext
+    {
+        public Formulario_Cadastro_PessoaContext (DbContextOptions<Formulario_Cadastro_PessoaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Formulario_Cadastro_Pessoa.Models.Cliente> Cliente { get; set; } = default!;
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Formulario_Cadastro_Pessoa.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id {  get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength (100)]
+        public string NomePai { get; set;}
+    }
+}
